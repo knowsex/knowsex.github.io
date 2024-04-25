76d6d1c49b2302cf50d468a5a87c04d9b41d5d0a
KnowSex <knowsex@outlook.com>
2024-04-26 02:10:37 +0800

    使用内容合集组织页面。

--- src/content/config.ts
+++ src/content/config.ts
@@ -15,7 +15,39 @@ const postsCollection = defineCollection({
       tags: z.array(z.string())
     })
 });
+
+const mainCollection = defineCollection({
+  type: 'content',
+  schema: z.object({
+    title: z.string(),
+    enTitle: z.string(),
+    desc: z.string().optional(),
+    keywords: z.array(z.string()).optional(),
+    bgPath: z.string().optional(),
+    authour: z.string().optional(),
+    review: z.string().optional(),
+    ifIndex: z.boolean().optional(),
+    isRoot: z.boolean().optional()
+  })
+});
+
+const generalCollection = defineCollection({
+  type: 'content',
+  schema: z.object({
+    title: z.string(),
+    enTitle: z.string().optional(),
+    desc: z.string().optional(),
+    keywords: z.array(z.string()).optional(),
+    bgPath: z.string().optional(),
+    authour: z.string().optional(),
+    review: z.string().optional(),
+    removeCover: z.boolean().optional(),
+  })
+});
+
 // 导出一个单独的 `collections` 对象来注册你的集合
 export const collections = {
   article: postsCollection,
+  main: mainCollection,
+  general: generalCollection
 };
