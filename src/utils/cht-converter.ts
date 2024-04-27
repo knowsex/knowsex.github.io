edd020f30d56d0f8deb2348453fb61842e4d2200
KnowSex <knowsex@outlook.com>
2024-04-27 16:55:47 +0800

    增加自定义简繁转换组。

--- src/utils/cht-converter.ts
+++ src/utils/cht-converter.ts
@@ -1,2 +1,14 @@
 import * as OpenCC from "opencc-js";
-export const convertToZH_TW = OpenCC.Converter({ from: "cn", to: "twp" });
+
+// 自定义转换组
+const customDict = [
+    ['“', '「'],
+    ['”', '」'],
+    ['‘', '『'],
+    ['’', '』'],
+];
+
+export const convertToZH_TW = OpenCC.ConverterFactory(
+    OpenCC.Locale.from.cn,                   // 大陆 => OpenCC 标准
+    OpenCC.Locale.to.twp.concat([customDict]) // OpenCC 标准 => 台湾+自订
+);
