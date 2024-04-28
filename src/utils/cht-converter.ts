f36362bf6973480b5bccb76a2b8284ac4c042656
KnowSex <knowsex@outlook.com>
2024-04-28 15:13:15 +0800

    修复转换顺序。

--- src/utils/cht-converter.ts
+++ src/utils/cht-converter.ts
@@ -6,9 +6,17 @@ const customDict = [
     ['”', '」'],
     ['‘', '『'],
     ['’', '』'],
+
+    // 生物学名词
+    ['艾滋病', '愛滋病'],
+    ['获得性免疫缺陷综合征', '後天免疫缺乏症候群'],
+    ['人类免疫缺陷病毒', '人類免疫缺乏病毒'],
+    ['尖锐湿疣', '尖銳濕疣'],
+    ['人乳头状瘤病毒', '人類乳突病毒']    
 ];
 
 export const convertToZH_TW = OpenCC.ConverterFactory(
-    OpenCC.Locale.from.cn,                   // 大陆 => OpenCC 标准
-    OpenCC.Locale.to.twp.concat([customDict]) // OpenCC 标准 => 台湾+自订
+    [customDict],
+    OpenCC.Locale.from.cn, // 大陆 => OpenCC 标准
+    OpenCC.Locale.to.twp   // OpenCC 标准 => 台湾+自订
 );
