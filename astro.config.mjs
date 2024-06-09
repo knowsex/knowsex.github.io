5d0a16d91c8029b72e75799f87a584d6b3123d7d
KnowSex <knowsex@outlook.com>
2024-06-10 03:13:59 +0800

    自动生成站点地图。

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,12 +1,12 @@
 import { defineConfig } from "astro/config";
 import { remarkModifiedTime } from "./remark-modified-time.mjs";
-
 import mdx from "@astrojs/mdx";
+import sitemap from "@astrojs/sitemap";
 
 // https://astro.build/config
 export default defineConfig({
   site: "https://knowsex.net",
-  integrations: [mdx()],
+  integrations: [mdx(), sitemap()],
   server: {
     host: true, // Allow LAN
   },
